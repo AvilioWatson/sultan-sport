efc580b491ba6802bd660dd2961fe7472f74a7b4
AvilioWatson <aviliowatson@gmail.com>
2025-09-10 00:09:29 +0700

    tugas 2

---
+++ main/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+
+import uuid
+from django.db import models
+
+class Product(models.Model):
+    PRODUCT_CHOICES = [
+        ('sepatu', 'Sransfer'),
+        ('baju', 'Baju'),
+        ('celana', 'Celana'),
+        ('raket', 'Raket'),
+        ('bola', 'Bola'),
+    ]
+
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    description = models.TextField()
+    address = models.TextField()
+    thumbnail = models.URLField(blank=True, null=True)
+    product = models.CharField(max_length=20, choices=PRODUCT_CHOICES, default='update')
+    is_featured = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
+    
+    @property
+    def is_news_hot(self):
+        return self.news_views > 20
+        
+    def increment_views(self):
+        self.news_views += 1
+        self.save()
